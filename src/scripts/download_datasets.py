18fcac0288911ec055af1603519c0743cc4ac695
Joachim Schrøder Andersson <joachimschroderandersson@Mac.lan>
2025-10-23 12:52:45 +0200

    Add script for evaluating speech models with Hydra configuration
    - Implemented a new script `download_datasets.py` to evaluate speech models.
    - Integrated Hydra for configuration management.
    - Added logging for dataset loading process.
    - Included utility functions for loading datasets and ignoring warnings.

---
+++ src/scripts/download_datasets.py
@@ -0,0 +1,42 @@
+"""Evaluate a speech model.
+
+Usage:
+    python src/scripts/evaluate_model.py [key=value] [key=value] ...
+"""
+
+import json
+
+from dotenv import load_dotenv
+import hydra
+from hydra.core.config_store import ConfigStore
+from hydra.core.hydra_config import HydraConfig
+from loguru import logger
+
+from utils.config_schema import ConfigSchema
+from utils.data import load_dataset_for_evaluation
+from utils.ignore_warnings import ignore_warnings
+
+
+cs = ConfigStore.instance()
+cs.store(name="config_schema", node=ConfigSchema)
+
+
+@hydra.main(config_path="../../config", config_name="config", version_base=None)
+def main(config: ConfigSchema) -> None:
+    """Evaluate a speech model on a dataset.
+
+    Args:
+        config:
+            The Hydra configuration object.
+    """
+
+    logger.info("Loading dataset...")
+    dataset = load_dataset_for_evaluation(config=config)
+    logger.info("Dataset loaded.")
+
+
+if __name__ == "__main__":
+    ignore_warnings()
+    load_dotenv()
+
+    main()
