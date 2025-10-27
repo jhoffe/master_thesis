4146dd06191bb6577b78d654d942c89f128a280d
Joachim Schrøder Andersson <joachimschroderandersson@Mac.lan>
2025-10-27 14:30:02 +0100

    Remove unused dataset and metrics definitions from make_dataset_plots.py

--- src/scripts/make_dataset_plots.py
+++ src/scripts/make_dataset_plots.py
@@ -5,19 +5,6 @@ from utils.dataset_descriptive_plots import (
 
 from loguru import logger
 
-DATASETS = {
-    "fleurs": "google--fleurs-da_dk-test-unfiltered",
-    "coral-v2": "CoRal-project--coral-v2-read_aloud-test-unfiltered",
-}
-
-
-METRICS = [
-    "clip_length",
-    "mean_pitch_hz",
-    "median_pitch_hz",
-    "voiced_ratio",
-]
-
 if __name__ == "__main__":
     logger.info("Generating dataset descriptive plots for CoRal-v2...")
     make_coral_plots()
