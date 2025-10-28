5608e88432120c2a558dd989fa5f58050ebf4202
Jonas Hoffmann <jhoffe.gh@proton.me>
2025-10-28 22:50:42 +0100

    Update model training configurations: adjust resource allocation in submit_model_finetuning.py, optimize learning rate and scheduling in adamw.yaml, modify dataset references in parakeet-tdt-0.6b-v3.yaml, and create new parakeet_training_set.yaml.

--- jobs/submit_model_finetuning.py
+++ jobs/submit_model_finetuning.py
@@ -14,11 +14,11 @@ def submit_job(config_name: str):
     opts = LSFSubmissionOptions(
         queue="gpua100",
         job_name=job_name,
-        num_cores=16,
+        num_cores=12,
         gpu_mode=GPUMode.EXCLUSIVE_PROCESS,
         gpu_num=1,
-        walltime="33:00",
-        memory="4GB",
+        walltime="32:00",
+        memory="5GB",
         working_directory=os.environ.get("HPC_PATH"),
         # Uncomment to direct outputs:
         output_file=f"logs/{job_name}.%J.out",
